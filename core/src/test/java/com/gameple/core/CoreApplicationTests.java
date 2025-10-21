6c3f6d0223cb36e3d78c15eb7b43f1225095691d
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2025-10-21 11:13:38 +0900

    Initial commit

---
+++ core/src/test/java/com/gameple/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameple.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
